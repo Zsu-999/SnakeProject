8d67a863d75a666e1568d3329ec0b8b7c8ddbba8
shaunhills <shaun.hills015@gmail.com>
2025-12-01 20:22:12 -0500

    Fix food position generation for snake game

--- updated food logic.c
+++ updated food logic.c
@@ -4,6 +4,6 @@ mvprintw(food.y, food.x, "*");
 // check if snake eats food
 if (snake[0].x == food.x && snake[0].y == food.y) {
     length++; // grow snake
-    food.x = rand() % (WIDTH -2)) + 1;
-    food.y = rand() % (HEIGHT -2)) + 1;
+    food.x = rand() % (WIDTH - 2)) + 1;
+    food.y = rand() % (HEIGHT - 2)) + 1;
 }
